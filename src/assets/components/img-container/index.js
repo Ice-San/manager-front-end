bb28461f35b114247d7ff1596b0dbf634c30d7a6
Ruben Costa <rubencosta.dev.2004@gmail.com>
2025-06-03 09:08:15 +0100

    MAN-7 | Front-End | SignIn page set as the default!

--- src/assets/components/img-container/index.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export const IconsContainer = ({ imgUrl, imgSize }) => {
-    return (_jsx("div", { className: imgSize, children: _jsx("div", { className: `divs-container ${imgUrl}` }) }));
-};
