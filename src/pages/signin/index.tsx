fb04c4d5134a395ee9bf95b9bcdc22982a5f481d
Ice-San <rotommine@gmail.com>
2025-07-18 01:01:59 +0100

    MAN-23 | Front-End | Add the toast when login get success or an error response!

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router';
 import { useForm } from 'react-hook-form';
+import { ToastContainer, toast } from 'react-toastify';
 
 import './index.css';
 
@@ -13,10 +14,12 @@ type FormData = {
 };
 
 export const SignInPage = () => {
-    const { register, handleSubmit } = useForm<FormData>();
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
     const navegate = useNavigate();
 
-    const onSubmit = handleSubmit(async dataForm => { 
+    const onSubmit = async (dataForm: FormData) => {
+        console.log(dataForm);
+
         try {
             const response = await fetch(`${VITE_API_ENDPOINT}/auth/signin`, {
                 method: "POST",
@@ -30,20 +33,47 @@ export const SignInPage = () => {
     
             if(status !== 200 || success !== true) {
                 console.error("Authentication was a failed...");
+                toast.error('Authentication was a failed...', {
+                    position: "bottom-right",
+                    pauseOnHover: false,
+                    draggable: 'touch'
+                });
                 return;
             }
 
             if(!token) {
-                console.error("User not exist!");
+                console.warn("User not exist!");
+                toast.warning('User not exist!', {
+                    position: "bottom-right",
+                    pauseOnHover: false,
+                    draggable: 'touch'
+                });
                 return;
             }
+
+            toast.success('Your Login was a success!', {
+                position: "bottom-right",
+                pauseOnHover: false,
+                draggable: 'touch'
+            });
             
             document.cookie = `token=${token};`;
             navegate("/account");
         } catch (err) {
             console.error("Something went wrong: ", err);
         }
-    });
+    };
+
+    function onError(err: any) {
+        Object.values(err).map((err: any) => {
+            toast.warning(err.message, {
+                position: "bottom-right",
+                pauseOnHover: false,
+                draggable: 'touch'
+            });
+            return;
+        });
+    }
 
     useEffect(() => {
         const decodedToken = decodeURIComponent(document.cookie);
@@ -62,20 +92,46 @@ export const SignInPage = () => {
                     <p>Enter your email below to login to your account</p>
                 </div>
 
-                <form onSubmit={onSubmit}>
+                <form onSubmit={handleSubmit(onSubmit, onError)}>
                     <div className="inputs-spacing">
                         <div className="input">
                             <div className="label">
                                 <h2>Email</h2>
                             </div>
-                            <input {...register('email', { required: 'It seems you dont\' put an email :( ', pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i, maxLength: 100 })} className='inputs' type="email" placeholder='m@example.com' />
+                            <input 
+                                {...register('email', { 
+                                    required: 'It seems you don\'t put an email', 
+                                    pattern: {
+                                        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i, 
+                                        message: "The email isn't valid!"
+                                    },
+                                    maxLength: {
+                                        value: 100, 
+                                        message: "You can only write 100 characters on email input!"
+                                    } 
+                                })} 
+                                className='inputs' 
+                                type="email" 
+                                placeholder='m@example.com' 
+                            />
                         </div>
 
                         <div className="input">
                             <div className="label">
                                 <h2>Password</h2>
                             </div>
-                            <input {...register('password', { required: 'It seems you dont\' put an password', maxLength: 100 })} className='inputs' type="password" placeholder='' />
+                            <input 
+                                {...register('password', { 
+                                    required: 'It seems you don\'t put an password', 
+                                    maxLength: {
+                                        value: 100, 
+                                        message: "You can only write 100 characters on email input!"
+                                    } 
+                                })}
+                                className='inputs' 
+                                type="password" 
+                                placeholder='' 
+                            />
                         </div>
                     </div>
 
@@ -84,6 +140,8 @@ export const SignInPage = () => {
                     </div>
                 </form>
             </div>
+
+            <ToastContainer />
         </div>
     );
 }
